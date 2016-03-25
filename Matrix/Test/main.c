cee898a04827f11289aa9ee6b53079610fd23bea
Pratik Shinde <pracshi@gmail.com>
2016-03-25 13:57:41 +0000

    Fixed a bug 	+ Address Calculation logic was wrong. Corrected it. 	+ Added some error handling.

--- Matrix/Test/main.c
+++ Matrix/Test/main.c
@@ -8,12 +8,13 @@ int main()
 	int row = 20;
 	int col = 20;
 	int i,j;
-	int read_row = 10;
-	int read_col = 15;
+	int read_row = 4;
+	int read_col = 5;
 	matrix = decl_matrix(row, col);
 
 	printf("Matrix Read:%d\n", read_matrix(matrix, read_row, read_col));
 	write_matrix(matrix, read_row, read_col, 1);
+	write_matrix(matrix, read_col, read_row, 1);
 	printf("After Matrix Read:%d\n", read_matrix(matrix, read_row, read_col));
 
 	for(i = 0; i< row; i++) {
--- Matrix/matrix.c
+++ Matrix/matrix.c
@@ -4,28 +4,67 @@
 #include "matrix.h"
 #include "bitmap.h"
 
+typedef struct header {
+	int row;
+	int col;
+} dimension_t;
+
+#define EXTRACT_DIM(a,b) (a = (dimension_t *)(b[1]));
+
 int **decl_matrix(int row, int col)
 {
-	int **matrix = (int **)malloc(sizeof(int*));
+	int **matrix;
+	dimension_t *dim;
 
-	*matrix = NULL;
-	*matrix = (int *)declare_bitmap(((row*col)/(sizeof(int)*8))+ 1);
+	matrix = (int **)malloc(sizeof(void *)*2);
+	if (matrix == NULL)
+		goto out;
 
+	matrix[0] = NULL;
+	matrix[0] = (int *)declare_bitmap(((row*col)/(sizeof(int)*8))+ 1);
+	if (matrix[0] == NULL) {
+		free(matrix);
+		matrix = NULL;
+		goto out;
+	}
+	/* Insert header: This dirty work has to be done to 
+         * to efficiently calculate the bit number in the function 
+         */
+	dim = (dimension_t *)malloc(sizeof(dimension_t));
+	if (dim == NULL) {
+		free(*matrix);
+		free(matrix);
+		matrix = NULL;
+		goto out;
+	}
+
+	dim->row = row;
+	dim->col = col;
+	matrix[1] = (void *)dim;
+out:
 	return matrix;
 }
 
 int read_matrix(int **matrix, int row, int col)
 {
-	return get_bit(*matrix, row*col);
+	dimension_t *dim;
+
+	EXTRACT_DIM(dim, matrix);
+
+	return get_bit(*matrix, (row*dim->col) + col);
 }
 
 void write_matrix(int **matrix, int row, int col, int data)
 {
-	write_bit(*matrix, row*col, data);
+	dimension_t *dim;
+	EXTRACT_DIM(dim, matrix);
+
+	write_bit(*matrix, (row*dim->col)+col, data);
 }
 
 void destroy_matrix(int **matrix)
 {
 	destroy_bitmap(*matrix);
+	free(matrix[1]);
 	free(matrix);
 }
